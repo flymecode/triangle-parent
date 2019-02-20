8e16969b87214f39eeab1b919f82d483572fbf1f
马旭 <1272193309@qq.com>
2019-02-20 13:41:54 +0800

    Initial commit

---
+++ triangle_common/src/main/java/com/xupt/common/entity/Result.java
@@ -0,0 +1,34 @@
+package com.xupt.common.entity;
+
+import lombok.Data;
+
+/**
+ * @author maxu
+ */
+@Data
+public class Result {
+	private boolean flag;//是否成功
+	private Integer code;// 返回码
+	private String message;//返回信息
+	private Object data;// 返回数据
+
+
+	public Result() {
+	}
+
+	public Result(boolean flag, Integer code, String message, Object
+			data) {
+		super();
+		this.flag = flag;
+		this.code = code;
+		this.message = message;
+		this.data = data;
+	}
+
+	public Result(boolean flag, Integer code, String message) {
+		super();
+		this.flag = flag;
+		this.code = code;
+		this.message = message;
+	}
+}
