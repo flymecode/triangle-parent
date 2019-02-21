ab5d360663d01def6c3f7dd18385eed7f8a9cb79
马旭 <1272193309@qq.com>
2019-02-21 21:30:14 +0800

    搭建短信服务

---
+++ triangle_sms/src/main/java/com/xupt/SmsApplication.java
@@ -0,0 +1,14 @@
+package com.xupt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author maxu
+ */
+@SpringBootApplication
+public class SmsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SmsApplication.class);
+	}
+}
