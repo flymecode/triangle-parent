613db1635f0e5de6c8b688d2d21bc1a29d8f57ab
马旭 <1272193309@qq.com>
2019-02-22 15:38:20 +0800

    添加条件查询

--- triangle_qa/src/main/java/com/xupt/qa/service/ProblemService.java
+++ triangle_qa/src/main/java/com/xupt/qa/service/ProblemService.java
@@ -1,5 +1,6 @@
 package com.xupt.qa.service;
 
+import com.xupt.common.untils.IdWorker;
 import com.xupt.qa.dao.ProblemDao;
 import com.xupt.qa.pojo.Problem;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -26,6 +28,9 @@ public class ProblemService {
 
 	@Autowired
 	private ProblemDao problemDao;
+
+	@Autowired
+	private IdWorker idWorker;
 
 	public Page<Problem> newlist(String labelid, int page, int size) {
 		Pageable pageable = new QPageRequest(page - 1, size);
@@ -54,11 +59,12 @@ public class ProblemService {
 	}
 
 	public void add(Problem problem) {
+		problem.setId(idWorker.nextId()+"" );
 		problemDao.save(problem);
 	}
 
 	public void update(Problem problem) {
-
+		problemDao.save(problem);
 	}
 
 	public void deleteById(String id) {
@@ -76,8 +82,38 @@ public class ProblemService {
 	private Specification<Problem> createSpecification(Map searchMap) {
 		return new Specification<Problem>() {
 			@Override
-			public Predicate toPredicate(Root<Problem> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
-				return null;
+			public Predicate toPredicate(Root<Problem> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				List<Predicate> predicateList = new ArrayList<Predicate>();
+				// ID
+				if (searchMap.get("id") != null && !"".equals(searchMap.get("id"))) {
+					predicateList.add(cb.like(root.get("id").as(String.class), "%" + (String) searchMap.get("id") + "%"));
+				}
+				// 标题
+				if (searchMap.get("title") != null && !"".equals(searchMap.get("title"))) {
+					predicateList.add(cb.like(root.get("title").as(String.class), "%" + (String) searchMap.get("title") + "%"));
+				}
+				// 内容
+				if (searchMap.get("content") != null && !"".equals(searchMap.get("content"))) {
+					predicateList.add(cb.like(root.get("content").as(String.class), "%" + (String) searchMap.get("content") + "%"));
+				}
+				// 用户ID
+				if (searchMap.get("userid") != null && !"".equals(searchMap.get("userid"))) {
+					predicateList.add(cb.like(root.get("userid").as(String.class), "%" + (String) searchMap.get("userid") + "%"));
+				}
+				// 昵称
+				if (searchMap.get("nickname") != null && !"".equals(searchMap.get("nickname"))) {
+					predicateList.add(cb.like(root.get("nickname").as(String.class), "%" + (String) searchMap.get("nickname") + "%"));
+				}
+				// 是否解决
+				if (searchMap.get("solve") != null && !"".equals(searchMap.get("solve"))) {
+					predicateList.add(cb.like(root.get("solve").as(String.class), "%" + (String) searchMap.get("solve") + "%"));
+				}
+				// 回复人昵称
+				if (searchMap.get("replyname") != null && !"".equals(searchMap.get("replyname"))) {
+					predicateList.add(cb.like(root.get("replyname").as(String.class), "%" + (String) searchMap.get("replyname") + "%"));
+				}
+
+				return cb.and(predicateList.toArray(new Predicate[predicateList.size()]));
 			}
 		};
 	}
